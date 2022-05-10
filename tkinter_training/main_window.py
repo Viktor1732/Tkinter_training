c7a7315f3c63a002818e6cd120095d24d2f0d3e2
Viktor1732 <programmer.kras@gmail.com>
2022-05-10 17:06:44 +0700

    Добавил LabelFrame. Заполнен текст, отредактировано расположение объектов.

--- tkinter_training/main_window.py
+++ tkinter_training/main_window.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from front_window import FrontWindow
+# from front_window import FrontWindow
 
 
 class Window:
@@ -19,28 +19,42 @@ class Window:
         self.draw_widgets()
         self.root.mainloop()
 
-    def create_front_window(self, title, width, height, icon=None):
-        FrontWindow(self.root, title, width, height, icon)
+    # def create_front_window(self, title, width, height, icon=None):
+    #     FrontWindow(self.root, title, width, height, icon)
 
     def draw_widgets(self):
+        frame_title = LabelFrame(
+            self.root, text="It's the LabelFrame!", relief=RAISED,
+            bg="yellow", font="TimesNewRoman 20", labelanchor=N
+        )
         frame_top = Frame(self.root)
-        frame_bottom = Frame(self.root)
+        frame_center = Frame(self.root)
+
+        frame_title.pack(padx=10, pady=10, ipadx=10, ipady=10, fill=BOTH)
         frame_top.pack()
-        frame_bottom.pack()
+        frame_center.pack()
+
+        Label(frame_title, text="Text number one!", relief=RAISED, border='5',
+              font='TimesNewRoman 15', height=3, width=15
+              ).pack(side=LEFT, padx=40, pady=10)
+
+        Label(frame_title, text="Text nuber two!", relief=RAISED, border='5',
+              font='TimesNewRoman 15', height=3, width=15
+              ).pack(side=RIGHT, padx=40, pady=10)
 
         Label(frame_top, image=self.picture1).pack(side=LEFT)
         Label(frame_top, image=self.picture2).pack(side=LEFT)
-        Label(frame_bottom, image=self.picture3).pack(side=LEFT)
-        Label(frame_bottom, image=self.picture4).pack(side=LEFT)
+        Label(frame_center, image=self.picture3).pack(side=LEFT)
+        Label(frame_center, image=self.picture4).pack(side=LEFT)
 
 
 if __name__ == "__main__":
     window = Window(
-        "It's the main window!", 600, 400,
+        "It's the main window!", 600, 580,
         r"C:\Users\vkryz\Desktop\tkinter_training\resources\iconka.ico"
     )
-    window.create_front_window(
-        "It's the front window!", 400, 200,
-        r"C:\Users\vkryz\Desktop\tkinter_training\resources\iconka.ico"
-    )
+#    window.create_front_window(
+#         "It's the front window!", 400, 200,
+#         r"C:\Users\vkryz\Desktop\tkinter_training\resources\iconka.ico"
+#    )
     window.run()
