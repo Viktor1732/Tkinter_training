653754c104d08cca56181ba5c653fb6e94924053
Viktor1732 <99543565+Viktor1732@users.noreply.github.com>
2022-05-13 08:58:25 +0700

    Добавлена кнопка с картинкой. Отредактировано расположение объектов.

--- tkinter_training/class button.py
+++ tkinter_training/class button.py
@@ -12,23 +12,31 @@ class Window:
             self.root.iconbitmap(icon)
         self.root.resizable(True, True)
 
-        self.label = Label(self.root, bg="green", width=85, height=30)
+        self.label = Label(self.root, bg="green")
+        self.photo_image = None
 
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
-        self.label.grid(padx=0, pady=0)
+        img = PilImage.open("butterfly.png")
+        img = img.resize((25, 25), PilImage.ANTIALIAS)
+        self.photo_image = ImageTk.PhotoImage(img)
 
-        Button(self.root, width=30, height=5, text="Button 1", bg="#FF2B2B", borderwidth="5",
+        self.label.grid()
+
+        Button(self.label, width=20, height=3, text="Button 1", bg="#FF2B2B", borderwidth="5",
                activebackground="black", activeforeground="white", command=lambda: self.change_color("red")
-               ).grid(column=0, row=0, sticky=NW, padx=15, pady=15)
-        Button(self.root, width=30, height=5, text="Button 2", bg="#2B2BFF", borderwidth="5",
+               ).grid(column=0, row=0, padx=15, pady=15)
+        Button(self.label, width=20, height=3, text="Button 2", bg="#2B2BFF", borderwidth="5",
                activebackground="black", activeforeground="white", command=lambda: self.change_color("blue")
-               ).grid(column=0, row=0, sticky=NE, padx=15, pady=15)
-        Button(self.root, text="Close", height=5, width=30, relief=RAISED, border="5",
-               command=quit).grid(column=0, row=0)
+               ).grid(column=2, row=0, padx=15, pady=15)
+        Button(self.label, text="Close", width=20, height=3, relief=RAISED, border="5",
+               command=quit).grid(column=1, row=1)
+
+        Button(self.label, width=150, height=40, text="  Какой-то текст!", image=self.photo_image,
+               compound=LEFT, activebackground="black", activeforeground='white').grid(column=1, row=2, pady=50)
 
     def change_color(self, color):
         self.label.configure(bg=color)
