94d80b8d12094111e3876f7b3080c60d8fb7b83b
Viktor1732 <programmer.kras@gmail.com>
2022-05-15 22:57:06 +0700

    Создал простейшее окно регистрации пользователя. Удалены предыдущие примеры файла treining_entry

--- tkinter_training/treining_entry.py
+++ tkinter_training/treining_entry.py
@@ -3,37 +3,48 @@ from tkinter import messagebox as mb
 
 
 class Window:
-    def __init__(self, title, width, height, icon=None):
+    def __init__(self, title, icon=None):
         self.root = Tk()
         self.root.title(title)
-        self.root.geometry(f"{width}x{height}+50+50")
         if icon is not None:
             self.root.iconbitmap(icon)
-        self.entry = Entry(self.root)
+
+        self.login_entry = Entry(self.root)
+        self.password_entry = Entry(self.root)
+        self.age_entry = Entry(self.root)
 
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
-        self.entry.configure(bg="pink", justify=CENTER, width=20)
-        self.entry.pack()
+        Label(self.root, text="Login").grid(column=0, row=0, sticky=W)
+        self.login_entry.configure(justify=CENTER)
+        self.login_entry.grid(column=1, row=0, sticky=W + S, padx=5, pady=2)
 
-        str_var_button = StringVar(value="OK")
-        Button(self.root, width=20, textvariable=str_var_button, command=self.get_enter).pack()
+        Label(self.root, text="Password").grid(column=0, row=1, sticky=W)
+        self.password_entry.configure(justify=CENTER)
+        self.password_entry.grid(column=1, row=1, sticky=W + S, padx=5, pady=2)
 
-        Button(self.root, text="Close", command=self.exit).pack()
+        Label(self.root, text="Age").grid(column=0, row=2, sticky=W)
+        self.age_entry.configure(justify=CENTER)
+        self.age_entry.grid(column=1, row=2, sticky=W + S, padx=5, pady=2)
 
-    def get_enter(self):
-        text = self.entry.get()
-        mb.showinfo("Entry text", text)
+        Button(self.root, text="Save", command=self.save, width=12).grid(column=0, row=3, padx=10)
 
-    def exit(self):
-        choice = mb.askyesno("QUIT", "Do you want to quit?")
+        Button(self.root, text="Close", command=self.close_window, width=12).grid(column=1, row=3, pady=10)
+
+    def save(self):
+        login = self.login_entry.get()
+        print(login)
+        mb.showinfo("Save", f"Hello {login}! I saved your date!")
+
+    def close_window(self):
+        choice = mb.askyesno("Quit", "Do you want exit?")
         if choice is True:
             self.root.destroy()
 
 
 if __name__ == "__main__":
-    window = Window("It's the main window!", 600, 400, r"iconka.ico")
+    window = Window("It's the main window!", r"iconka.ico")
     window.run()
