9b006fee2762ff2adad81d7a5bb7f6ee0d3547ce
Viktor1732 <99543565+Viktor1732@users.noreply.github.com>
2022-05-18 10:31:08 +0700

    Добавлен выбор цвета при помощи spinbox, информация о выбранном цвете.

--- tkinter_training/spinbox.py
+++ tkinter_training/spinbox.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import messagebox as mb
 
+list_colors = sorted(["gold", "silver", "crimson", "purple", "green", "orange",
+                      "chocolate", "coral", "blue"])
+
 
 class Window:
     def __init__(self, title, icon=None):
@@ -11,12 +14,31 @@ class Window:
         self.root.resizable(True, True)
         self.root.geometry("350x200")
 
+        self.choice_color = Spinbox(self.root, values=list_colors, wrap=True, state="readonly", command=self.get_color)
+        self.header_name = Label(self.root, text="Background color", font=("Calibri", 15))
+
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
+        self.root.configure(bg="blue")
+        self.header_name.configure(bg="blue")
+        self.header_name.pack()
+        self.choice_color.pack(pady=30)
+
+        Button(self.root, text="Color info", width=20, command=self.get_color_name).pack(pady=10)
         Button(self.root, text="Close", width=20, command=self.exit).pack()
+
+    def get_color(self):
+        color = self.choice_color.get()
+        self.root.configure(bg=color)
+        self.header_name.configure(bg=color)
+
+    def get_color_name(self):
+        color_name = self.choice_color.get()
+        up_string = color_name.upper()
+        mb.showinfo("Info color", f"Selected color:  {up_string}")
 
     def exit(self):
         choice = mb.askyesno("Quit", "Do you want quit?")
