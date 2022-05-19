4a7bf47cf5eff3851df8d73ded1894815a4fa6b2
Viktor1732 <99543565+Viktor1732@users.noreply.github.com>
2022-05-19 10:41:00 +0700

    Добавлены меню fale, edit, view. По пути "file/setting/change_color" добавлен выбор цвета.

--- tkinter_training/menu.py
+++ tkinter_training/menu.py
@@ -10,14 +10,70 @@ class Window:
             self.root.iconbitmap(icon)
         self.root.resizable(True, True)
         self.root.geometry("350x200")
+        self.color_font = IntVar()
+        self.root.configure(bg="red")
 
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
-
+        self.draw_manu()
         Button(self.root, text="Close", width=20, command=self.exit).pack()
+
+    def draw_manu(self):
+        menu_bar = Menu(self.root)
+
+        file_menu = Menu(menu_bar, tearoff=False)
+        file_menu.add_command(label="New project")
+
+        setting_file_menu = Menu(file_menu, tearoff=False)
+        file_menu.add_cascade(menu=setting_file_menu, label="Setting")
+
+        change_color_setting_file_menu = Menu(setting_file_menu, tearoff=False)
+
+        change_color_setting_file_menu.add_radiobutton(
+            label="red", value=0, variable=self.color_font, command=self.change_color
+        )
+        change_color_setting_file_menu.add_radiobutton(
+            label="green", value=1, variable=self.color_font, command=self.change_color
+        )
+        change_color_setting_file_menu.add_radiobutton(
+            label="yellow", value=2, variable=self.color_font, command=self.change_color
+        )
+        setting_file_menu.add_cascade(menu=change_color_setting_file_menu, label="Change color")
+
+        file_menu.add_separator()
+        file_menu.add_command(label="Close")
+        menu_bar.add_cascade(menu=file_menu, label="File")
+
+        edit_menu = Menu(menu_bar, tearoff=False)
+        edit_menu.add_command(label="Copy", underline=0)
+        edit_menu.add_command(label="Paste")
+        edit_menu.add_command(label="Delete")
+        menu_bar.add_cascade(menu=edit_menu, label="Edit")
+
+        view_menu = Menu(menu_bar, tearoff=False)
+        tool_window_menu = Menu(view_menu, tearoff=False)
+        tool_window_menu.add_command(label="Commit")
+        tool_window_menu.add_command(label="Project")
+        tool_window_menu.add_command(label="Find")
+
+        view_menu.add_cascade(menu=tool_window_menu, label="Tool Window")
+        view_menu.add_command(label="Appearance")
+        view_menu.add_separator()
+        view_menu.add_command(label="Quick Definition")
+        menu_bar.add_cascade(menu=view_menu, label="View")
+
+        self.root.configure(menu=menu_bar)
+
+    def change_color(self):
+        if self.color_font.get() == 0:
+            self.root.configure(bg="red")
+        if self.color_font.get() == 1:
+            self.root.configure(bg="green")
+        if self.color_font.get() == 2:
+            self.root.configure(bg="yellow")
 
     def exit(self):
         choice = mb.askyesno("Quit", "Do you want quit?")
@@ -26,5 +82,5 @@ class Window:
 
 
 if __name__ == "__main__":
-    window = Window("Сhange the background color!", r"iconka.ico")
+    window = Window("It's a menu training!", r"iconka.ico")
     window.run()
