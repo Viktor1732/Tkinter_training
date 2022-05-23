7709c8c3ede1c6679bd6fc320b6de43a254c3f34
Viktor1732 <programmer.kras@gmail.com>
2022-05-23 23:54:03 +0700

    Прописан функционал file-меню. Добавлены функции к меню: open, open_dir, save.

--- tkinter_training/filedialog.py
+++ tkinter_training/filedialog.py
@@ -1,7 +1,7 @@
 from tkinter import *
-from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter.scrolledtext import ScrolledText
 
 
 class Window:
@@ -29,14 +29,42 @@ class Window:
         menu_bar = Menu(self.root)
 
         file_menu = Menu(self.root, tearoff=False)
-        file_menu.add_command(label="open")
-        file_menu.add_command(label="save")
-        file_menu.add_command(label="save as...")
+        file_menu.add_command(label="open", command=self.open_file)
+        file_menu.add_command(label="open dir", command=self.open_dir)
+        file_menu.add_command(label="save as...", command=self.save)
         file_menu.add_separator()
         file_menu.add_command(label="quit", command=self.exit)
         menu_bar.add_cascade(menu=file_menu, label="File")
 
         self.root.configure(menu=menu_bar)
+
+    def open_file(self):
+        # file_name = filedialog.askopenfilename()
+        # if file_name:
+        #     self.txt.insert(END, f"Файил открыт по пути: {file_name}\n")
+        #     with open(file_name, "r") as r:
+        #         self.txt.insert(END, str(r.read()))
+
+        name = filedialog.askopenfile()
+        if name:
+            self.txt.insert(END, name.read())
+            name.close()
+
+    def open_dir(self):
+        path = filedialog.askdirectory(mustexist=True)
+        self.txt.insert(END, f"Вы открыли папку: {path}")
+
+    def save(self):
+        # file_name = filedialog.askopenfilename()
+        # if file_name:
+        #     with open(file_name, 'w') as r:
+        #         r.write("1234567")
+
+        file = filedialog.asksaveasfile(filetypes=(("TEXT", "*.txt"), ("ALL FILES", "*.*")))
+        if file:
+            file.write("134567799999")
+            file.close()
+            self.txt.insert(END, "Фаил записан!\n")
 
     def exit(self):
         choice = messagebox.askyesno("Quit", 'Do you want quit?')
